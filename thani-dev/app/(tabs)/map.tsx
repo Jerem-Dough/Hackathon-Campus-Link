68e74569efb6fe922a809751713e30397599ec91
Alisha <apravasi@Alishas-MacBook-Pro-473.local>
2025-04-26 13:51:44 -0600

    AP: Initial map loading in (using MapBox)

--- thani-dev/app/(tabs)/map.tsx
+++ thani-dev/app/(tabs)/map.tsx
@@ -1,12 +1,10 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, View } from 'react-native';
+import MapController from '@/components/mapController'; 
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Campus Map</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>Find events and transport options here!</Text>
+      <MapController />
     </View>
   );
 }
@@ -14,17 +12,5 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 20,
-    height: 1,
-    width: '80%',
   },
 });
