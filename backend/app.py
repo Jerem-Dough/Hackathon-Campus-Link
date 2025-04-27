08cce072155318aeb817c6df6b5f13c6c0b60bbf
Jeremy <jerem.dougherty@gmail.com>
2025-04-26 21:37:36 -0600

    Added CORS for Frontend access!

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.events import events_bp
 from routes.users import users_bp
 from routes.forums import forums_bp
 
 app = Flask(__name__)
+CORS(app)
 
 # Register blueprints
 app.register_blueprint(events_bp)
